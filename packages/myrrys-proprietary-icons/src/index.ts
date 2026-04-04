37724b8aa59a848fa88c54f96e26283a2dbce4ec
Ville Takanen <vitkukissa@gmail.com>
2026-04-04 20:49:30 +0300

    chore: remove deprecated myrrys-proprietary-icons package

--- packages/myrrys-proprietary-icons/src/index.ts
+++
@@ -1,9 +0,0 @@
-/**
- * @myrrys/proprietary-icons - Tier 2 Managed Non-MIT Icon Registry.
- */
-
-export const icons = {
-  // Empty for bootstrap, but ready for proprietary/fair-use additions.
-} as const;
-
-export type IconNoun = keyof typeof icons;
